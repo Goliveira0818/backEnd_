46aa901bc9a3f3b18fc980f9dd625d591e4ec19c
GUSTAVO HENRIQUE OLIVEIRA DOS SANTOS <58115162825@info.local>
2025-10-21 15:58:23 -0300

    .

---
+++ DesafioCalculadora/Program.cs
@@ -0,0 +1,118 @@
+﻿double n1, n2, operacao, continuar;
+
+do
+{
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("-------------------------");
+    Console.WriteLine("|                       |");
+    Console.WriteLine("|      Calculadora      |");
+    Console.WriteLine("|          com          |");
+    Console.WriteLine("|         menu          |");
+    Console.WriteLine("|                       |");
+    Console.WriteLine("-------------------------");
+    Console.WriteLine("");
+    Console.WriteLine("");
+
+    Console.WriteLine("Digite o número da operação:");
+    Console.WriteLine("1-Soma");
+    Console.WriteLine("2-Subtração");
+    Console.WriteLine("3-Multiplicação");
+    Console.WriteLine("4-Divisão");
+    Console.WriteLine("0-Sair");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    operacao = double.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    Console.WriteLine("");
+    if (operacao == 0)
+    {
+        sair();
+    }
+    else
+    {
+        Console.WriteLine("Digite o primeiro número:");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        n1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+
+        Console.WriteLine("Digite o segundo número:");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        n2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        switch (operacao)
+        {
+            case 1:
+                soma();
+                break;
+            case 2:
+                subtração();
+                break;
+            case 3:
+                multiplicação();
+                break;
+            case 4:
+                divisão();
+                break;
+            default:
+                invalida();
+                break;
+        }
+    Thread.Sleep(1000);
+    }
+} while (operacao != 0);
+
+
+
+void sair()
+{
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("-------------------------");
+    Console.WriteLine("|    Até mais Usuario   |");
+    Console.WriteLine("-------------------------");
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
+
+void soma()
+{
+    double resultado = n1 + n2;
+    Console.WriteLine((n1) + " + " + (n2) + " = " + (resultado));
+}
+
+void subtração()
+{
+    double resultado = n1 - n2;
+    Console.WriteLine((n1) + " - " + (n2) + " = " + (resultado));
+}
+
+void multiplicação()
+{
+    double resultado = n1 * n2;
+    Console.WriteLine((n1) + " * " + (n2) + " = " + (resultado));
+}
+
+void divisão()
+{
+    if (n2 == 0 || n1 == 0)
+    {
+        Console.WriteLine("ERRO: IMPOSSÍVEL DIVIDIR POR 0");
+    }
+    else
+    {
+        double resultado = n1 / n2;
+        Console.WriteLine((n1) + " / " + (n2) + " = " + (resultado));
+    }
+}
+
+void invalida()
+{
+    Console.WriteLine("ERRO: Operação inválida");
+}
---
+++ funcoes/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine($"Digite seu nome");
+string nome = Console.ReadLine();
+Console.WriteLine($"Digite seu nome");
+string sobrenome = Console.ReadLine();
+
+
+Console.WriteLine($"ok, {nome}! Agora vamos dar as boas vindas a você!!");
+
+Console.WriteLine($"por favor escolha uma opção abaixo:");
+Console.WriteLine($"1 - Saudar com nome");
+Console.WriteLine($"2 - saudar com o sobrenome");
+Console.WriteLine($"3 - saudar com o nome e sobrenome");
+Console.WriteLine($"0 - Apenas saudação genérica");
+
+int opcao = 0;
+switch (opcao)
+{
+    case 0:
+        saudacaoGenerica();
+
+        break;
+    case 1:
+        SaudarComNome(nome);
+        break;
+
+    case 2:
+        SaudarComSobrenome(sobrenome);
+        break;
+
+    case 3:
+        string nomeComplrto = DevolveNomeCompleto(nome, sobrenome);
+        Console.WriteLine($"Seja bem vindo {nomeCompleto}");
+        break;
+    default:
+        break;
+}
+
+
+void saudacaoGenerica()
+{
+    Console.WriteLine($"Olá, seja bem vindo ao nosso programa, tenha um ótimo dia :)");
+}
+
+void SaudarComNome(string nomeRecebido)
+{
+    Console.WriteLine($"Seja bem vindo, {nomeRecebido}");
+}
+
+void SaudarComSobrenome(string sobrenomeRecebido)
+{
+    Console.WriteLine($"Olá, seja bem-vindo {sobrenomeRecebido}");
+}
+
+string DevolveNomeCompleto(string nomeRecebido, string sobrenomeRecebido)
+{
+    return $"{nomeRecebido} {sobrenomeRecebido}";
+}
